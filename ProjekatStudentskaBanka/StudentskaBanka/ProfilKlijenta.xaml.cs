c464167bb4ef3b2ac75bc4a5ff464ebfb015321f
ahmedmalkoc <amalkoc1@etf.unsa.ba>
2018-05-08 18:06:20 +0200

    popravka forme ProfilKlijenta Signed-off-by: ahmedmalkoc <amalkoc1@etf.unsa.ba>

--- ProjekatStudentskaBanka/StudentskaBanka/ProfilKlijenta.xaml.cs
+++ ProjekatStudentskaBanka/StudentskaBanka/ProfilKlijenta.xaml.cs
@@ -20,9 +20,9 @@ namespace StudentskaBanka
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class ProfilKlijenta : Page
     {
-        public BlankPage1()
+        public ProfilKlijenta()
         {
             this.InitializeComponent();
         }
