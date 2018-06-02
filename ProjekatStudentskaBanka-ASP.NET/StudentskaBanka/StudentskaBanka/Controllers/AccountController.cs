ad57eddc910c620dcd6c15e75f31227b3a6c0ff1
ahmedmalkoc <amalkoc1@etf.unsa.ba>
2018-06-02 05:25:17 +0200

    pocetak web aplikacije Signed-off-by: ahmedmalkoc <amalkoc1@etf.unsa.ba>

---
+++ ProjekatStudentskaBanka-ASP.NET/StudentskaBanka/StudentskaBanka/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentskaBanka.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+    }
+}
---
+++ ProjekatStudentskaBanka-ASP.NET/StudentskaBanka/StudentskaBanka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentskaBanka.MvcApplication" Language="C#" %>
