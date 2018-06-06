d60dc042cf3c09f4f54d533abb91d93e5e50d42f
ahmedmalkoc <amalkoc1@etf.unsa.ba>
2018-06-06 19:01:30 +0200

    dodana specificna funkcionalnost - mapa Signed-off-by: ahmedmalkoc <amalkoc1@etf.unsa.ba>

--- ProjekatStudentskaBanka-ASP.NET/StudentskaBanka/StudentskaBanka/Controllers/HomeController.cs
+++ ProjekatStudentskaBanka-ASP.NET/StudentskaBanka/StudentskaBanka/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace StudentskaBanka.Controllers
 {
@@ -27,5 +30,52 @@ namespace StudentskaBanka.Controllers
             return View();
         }
 
+        public ActionResult MapaSpecificFunctionality()
+        {
+            ViewBag.Message = "Vaza lokacija";
+
+            return View();
+        }
+
+        public ActionResult GetLocations()
+        {
+            var task = Task.Run(async () => await AsyncUzmiLokacije());
+            task.Wait();
+            var asyncFunctionResult = task.Result;
+            return asyncFunctionResult;
+        }
+
+        public async Task<ActionResult> AsyncUzmiLokacije()
+        {
+
+            var client = new HttpClient();
+            var address = new Uri("https://api.foursquare.com/v2/venues/search?ll=43.85,18.23&categoryId=4bf58dd8d48988d175941735&client_id=KHAWRYD4PJ0LKVSZQF4CEXTX5GK3BDPTWS3XLCTVAYQPK515&client_secret=BSTBTSNVENYHWGGNYGGQ00X33NJNNFTPZVIPOB3LGC1UVXBI&v=20160202");
+            HttpResponseMessage response = await client.GetAsync(address);
+            String stream = await response.Content.ReadAsStringAsync();
+            dynamic dyn = JsonConvert.DeserializeObject(stream);
+            var sp = stream.Split('"');
+            double lat = 0;
+            var locations = new List<Lokacija>();
+            string loc = "n";
+            for (int i = 0; i < sp.Length; i++)
+            {
+                if (sp[i] == "lat")
+                    lat = Convert.ToDouble(sp[i + 1].Substring(1, sp[i + 1].Length - 2));
+                if (sp[i] == "lng")
+                {
+                    for (int j = i; j >= 0; j--)
+                        if (sp[j] == "name")
+                        {
+                            loc = sp[j + 2];
+                            break;
+                        }
+                    locations.Add(new Lokacija(lat, Convert.ToDouble(sp[i + 1].Substring(1, sp[i + 1].Length - 4)), loc));
+                }
+            }
+
+            return Json(locations, JsonRequestBehavior.AllowGet);
+        }
+    
     }
+
 }
---
+++ ProjekatStudentskaBanka-ASP.NET/StudentskaBanka/StudentskaBanka/Controllers/Lokacija.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentskaBanka.Controllers
+{
+    public class Lokacija
+    {
+        public double geografskaSirina { get; set; }
+        public double geografskaDuzina { get; set; }
+        public string naziv { get; set; }
+
+        public Lokacija(double geografskaSirina, double geografskaDuzina, string naziv)
+        {
+            this.geografskaSirina = geografskaSirina;
+            this.geografskaDuzina = geografskaDuzina;
+            this.naziv = naziv;
+        }
+    }
+}
